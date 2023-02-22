da89520daeee20da1341929f9b2b62b8b51bc7ea
deutschkihun@gmail.com <deutschkihun@gmail.com>
2023-02-22 10:28:05 +0900

    add toss-card

---
+++ toss-card/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:layout/screens/card_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+        statusBarColor: Colors.white, statusBarBrightness: Brightness.dark));
+
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: CardPage());
+  }
+}
